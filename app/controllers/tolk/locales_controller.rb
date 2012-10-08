2d9d55f7cec21b9769bbdcf6b4eaf00b75e6fcfc
chytreg <dariusz.gertych@gmail.com>
2012-10-08 12:25:31 +0200

    allow to edit primary locale

--- app/controllers/tolk/locales_controller.rb
+++ app/controllers/tolk/locales_controller.rb
@@ -1,12 +1,12 @@
 module Tolk
   class LocalesController < Tolk::ApplicationController
     before_filter :find_locale, :only => [:show, :all, :update, :updated]
-    before_filter :ensure_no_primary_locale, :only => [:all, :update, :show, :updated]
+    # before_filter :ensure_no_primary_locale, :only => [:all, :update, :show, :updated]
 
     def index
       @locales = Tolk::Locale.secondary_locales.sort_by(&:language_name)
     end
-  
+
     def show
       respond_to do |format|
         format.html do
